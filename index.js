b9cedb1a14e078f12732bb8567e5aa62fb7934e3
Tomer Ohana <tomero@wix.com>
2018-01-16 14:51:57 +0200

    Add support for dynamic import

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const env = process.env.BABEL_ENV || process.env.NODE_ENV;
 const basePresets = ['react', 'stage-2'];
+const basePlugins = ['syntax-dynamic-import'];
+const testPlugins = env === 'test' ? ['transform-dynamic-import'] : [];
 const envPreset = env === 'test' ? ['env', {targets: {node: 'current'}}] : 'env';
 const presets = [envPreset].concat(basePresets);
-module.exports = {presets};
+const plugins = basePlugins.concat(testPlugins);
+module.exports = {presets, plugins};
