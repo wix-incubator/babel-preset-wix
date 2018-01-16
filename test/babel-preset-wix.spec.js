aad618f0629bd2ea0e320b00e3d50e7fa7d2bac1
Tomer Ohana <tomero@wix.com>
2018-01-16 16:30:57 +0200

    Add support for dynamic import (#1)

--- test/babel-preset-wix.spec.js
+++ test/babel-preset-wix.spec.js
@@ -5,23 +5,35 @@ describe('babel-preset-wix', () => {
 
   it('should return env preset in development mode', () => {
     process.env.NODE_ENV = 'development';
-    expect(require('../index')).to.eql({presets: ['env', 'react', 'stage-2']});
+    expect(require('../index')).to.eql({
+      presets: ['env', 'react', 'stage-2'],
+      plugins: ['syntax-dynamic-import']
+    });
   });
 
   it('should return env preset in production mode', () => {
     process.env.NODE_ENV = 'production';
-    expect(require('../index')).to.eql({presets: ['env', 'react', 'stage-2']});
+    expect(require('../index')).to.eql({
+      presets: ['env', 'react', 'stage-2'],
+      plugins: ['syntax-dynamic-import']
+    });
   });
 
   it('should return env preset in test mode', () => {
     process.env.NODE_ENV = 'test';
-    expect(require('../index')).to.eql({presets: [['env', {targets: {node: 'current'}}], 'react', 'stage-2']});
+    expect(require('../index')).to.eql({
+      presets: [['env', {targets: {node: 'current'}}], 'react', 'stage-2'],
+      plugins: ['syntax-dynamic-import', 'transform-dynamic-import']
+    });
   });
 
   it('should prefer BABEL_ENV over NODE_ENV', () => {
     process.env.NODE_ENV = 'test';
     process.env.BABEL_ENV = 'development';
-    expect(require('../index')).to.eql({presets: ['env', 'react', 'stage-2']});
+    expect(require('../index')).to.eql({
+      presets: ['env', 'react', 'stage-2'],
+      plugins: ['syntax-dynamic-import']
+    });
   });
 });
 
