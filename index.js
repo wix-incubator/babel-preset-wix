270bfa27e555f9bdc281cfc621c2ae8f05a61d9d
Shahar Talmi <shahar.talmi@gmail.com>
2018-05-18 16:55:21 +0300

    added support for es modules

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const env = process.env.BABEL_ENV || process.env.NODE_ENV;
 const basePresets = ['react', 'stage-2'];
 const basePlugins = ['syntax-dynamic-import'];
 const testPlugins = env === 'test' ? ['transform-dynamic-import'] : [];
-const envPreset = env === 'test' ? ['env', {targets: {node: 'current'}}] : 'env';
+const modules = require(path.resolve('package.json')).module ? false : 'commonjs';
+const envPreset = env === 'test' ? ['env', {targets: {node: 'current'}, modules}] : ['env', {modules}];
 const presets = [envPreset].concat(basePresets);
 const plugins = basePlugins.concat(testPlugins);
 module.exports = {presets, plugins};
--- test/babel-preset-wix.spec.js
+++ test/babel-preset-wix.spec.js
@@ -1,12 +1,18 @@
 const {expect} = require('chai');
+const path = require('path');
 
 describe('babel-preset-wix', () => {
-  afterEach(() => delete require.cache[require.resolve('../index')]);
+  let pwd;
+  beforeEach(() => pwd = process.cwd());
+  afterEach(() => {
+    delete require.cache[require.resolve('../index')];
+    process.chdir(pwd);
+  });
 
   it('should return env preset in development mode', () => {
     process.env.NODE_ENV = 'development';
     expect(require('../index')).to.eql({
-      presets: ['env', 'react', 'stage-2'],
+      presets: [['env', {modules: 'commonjs'}], 'react', 'stage-2'],
       plugins: ['syntax-dynamic-import']
     });
   });
@@ -14,7 +20,7 @@ describe('babel-preset-wix', () => {
   it('should return env preset in production mode', () => {
     process.env.NODE_ENV = 'production';
     expect(require('../index')).to.eql({
-      presets: ['env', 'react', 'stage-2'],
+      presets: [['env', {modules: 'commonjs'}], 'react', 'stage-2'],
       plugins: ['syntax-dynamic-import']
     });
   });
@@ -22,7 +28,7 @@ describe('babel-preset-wix', () => {
   it('should return env preset in test mode', () => {
     process.env.NODE_ENV = 'test';
     expect(require('../index')).to.eql({
-      presets: [['env', {targets: {node: 'current'}}], 'react', 'stage-2'],
+      presets: [['env', {targets: {node: 'current'}, modules: 'commonjs'}], 'react', 'stage-2'],
       plugins: ['syntax-dynamic-import', 'transform-dynamic-import']
     });
   });
@@ -31,7 +37,16 @@ describe('babel-preset-wix', () => {
     process.env.NODE_ENV = 'test';
     process.env.BABEL_ENV = 'development';
     expect(require('../index')).to.eql({
-      presets: ['env', 'react', 'stage-2'],
+      presets: [['env', {modules: 'commonjs'}], 'react', 'stage-2'],
+      plugins: ['syntax-dynamic-import']
+    });
+  });
+
+  it('should not transpile imports in case package declares module', () => {
+    process.env.NODE_ENV = 'production';
+    process.chdir(path.join(__dirname, 'es-modules-test-dir'));
+    expect(require('../index')).to.eql({
+      presets: [['env', {modules: false}], 'react', 'stage-2'],
       plugins: ['syntax-dynamic-import']
     });
   });
