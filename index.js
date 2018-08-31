ab0c9fe1602d8b9b281b9ca1f3cd977a953a74ed
Andrii Popovych <andriip@wix.com>
2018-08-31 04:40:16 +0300

    Make preset configurable through options

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ const basePresets = ['react', 'stage-2'];
 const basePlugins = ['transform-decorators-legacy', 'syntax-dynamic-import'];
 const testPlugins = env === 'test' ? ['dynamic-import-node'] : [];
 const pkg = require(path.resolve('package.json'));
-const modules = process.env.IN_WEBPACK === 'true' || pkg.module ? false : 'commonjs';
-const envPreset = env === 'test' ? ['env', {targets: {node: 'current'}, modules}] : ['env', {modules}];
-const presets = [envPreset].concat(basePresets);
-const plugins = basePlugins.concat(testPlugins);
-module.exports = {presets, plugins};
+
+function buildPreset(context, opts = {}) {
+  const envPresetOptions = {};
+  envPresetOptions.modules = process.env.IN_WEBPACK === 'true' || pkg.module ? false : 'commonjs';
+  if (typeof opts.loose !== 'undefined') {
+    envPresetOptions.loose = opts.loose;
+  }
+  if (typeof opts.useBuiltIns !== 'undefined') {
+    envPresetOptions.useBuiltIns = opts.useBuiltIns;
+  }
+  const envPreset = env === 'test' ?
+    ['env', Object.assign({targets: {node: 'current'}}, envPresetOptions)] :
+    ['env', envPresetOptions];
+  const presets = [envPreset].concat(basePresets);
+  const plugins = basePlugins.concat(testPlugins);
+  return {presets, plugins};
+}
+
+module.exports = buildPreset({});
+
+Object.defineProperty(module.exports, 'buildPreset', {
+  configurable: true,
+  writable: true,
+  enumerable: false,
+  value: buildPreset
+});
