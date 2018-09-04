b56c19686c7292387571568b196b34d9f470c409
Andrew Popovych <piratus@gmail.com>
2018-09-04 10:22:30 +0300

    Use `dynamic-import-node` plugin instead of `transform-dynamic-import` (#3)
    `babel-plugin-transform-dynamic-import` is a babel 7 plugin, while others are babel 6.
    
    Using `babel-plugin-dynamic-import-node` eliminates npm warnings during install.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const env = process.env.BABEL_ENV || process.env.NODE_ENV;
 const basePresets = ['react', 'stage-2'];
 const basePlugins = ['transform-decorators-legacy', 'syntax-dynamic-import'];
-const testPlugins = env === 'test' ? ['transform-dynamic-import'] : [];
+const testPlugins = env === 'test' ? ['dynamic-import-node'] : [];
 const pkg = require(path.resolve('package.json'));
 const modules = process.env.IN_WEBPACK === 'true' || pkg.module ? false : 'commonjs';
 const envPreset = env === 'test' ? ['env', {targets: {node: 'current'}, modules}] : ['env', {modules}];
